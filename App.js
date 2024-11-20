ff5a2e20db7435983cc9b00e6fdc474edd1ceb11
TDMinhNhat <tdminhnhat13092003@gmail>
2024-11-20 20:55:00 +0700

    Upload week08

---
+++ App.js
@@ -0,0 +1,29 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Screen02 from './components/Screen02';
+import Screen01 from './components/Screen01';
+import Screen03 from './components/Screen03';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer >
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+       <Stack.Screen
+          name="Screen01"
+          component={Screen03}
+        />
+        <Stack.Screen
+          name="Screen03"
+          component={Screen02}
+        />
+        <Stack.Screen
+          name="Screen02"
+          component={Screen01}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
